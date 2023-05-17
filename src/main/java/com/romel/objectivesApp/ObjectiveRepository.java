fbbb3db509746ad29f01706c53536ab92af59c6c
romelburcea <113209485+romelburcea@users.noreply.github.com>
2023-05-17 20:42:04 +0300

    Delete src directory

--- src/main/java/com/romel/objectivesApp/ObjectiveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.romel.objectivesApp;
-
-import com.romel.objectivesApp.models.Objective;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ObjectiveRepository extends JpaRepository<Objective, Long> {
-}
