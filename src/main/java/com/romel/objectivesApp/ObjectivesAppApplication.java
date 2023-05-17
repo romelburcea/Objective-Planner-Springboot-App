cf31cf6a5d948ea24779c009a4dca61652a683ac
romelburcea <113209485+romelburcea@users.noreply.github.com>
2023-05-17 20:44:30 +0300

    Add files via upload

---
+++ src/main/java/com/romel/objectivesApp/ObjectivesAppApplication.java
@@ -0,0 +1,13 @@
+package com.romel.objectivesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectivesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectivesAppApplication.class, args);
+	}
+
+}
